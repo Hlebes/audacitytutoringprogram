41ce664cbc1301d5de2b9ffbaab1f392cd3edc58
Gleb <glebdmitrienko@gmail.com>
2021-11-29 10:01:50 +0300

    Add files via upload

--- RtfImportTest/RtfImportTest/MainWindow.xaml.cs
+++ RtfImportTest/RtfImportTest/MainWindow.xaml.cs
@@ -26,32 +26,56 @@ namespace RtfImportTest
         string materialChoice = "";
         string docPath = "";
         object maincontent;
-
+        RadioButton[] radiobuttons;
+        CheckBox[] checkbuttons;
+        int currentQuizId = 0;
+        Quiz currentQuiz;
 
         public MainWindow()
         {
             InitializeComponent();
             maincontent = this.Content;
+
+            SerializeQuestions();
+
+            //Десериализация списка 
+            List<Quiz> returnedQuestions = new List<Quiz>();
+            using (var reader = new StreamReader("quiz.xml"))
+            {
+                XmlSerializer deserializer = new XmlSerializer(returnedQuestions.GetType(),
+                    new XmlRootAttribute("Quiz"));
+                returnedQuestions = (List<Quiz>)deserializer.Deserialize(reader);
+            }
         }
 
         private void Window_Loaded(object sender, RoutedEventArgs e)
         {
-            
+            RadioButton[] quizRB = new RadioButton[] { (RadioButton)this.FindName("quizButton1"),
+                                                       (RadioButton)this.FindName("quizButton2"),
+                                                       (RadioButton)this.FindName("quizButton3"),
+                                                       (RadioButton)this.FindName("quizButton4")};
+            radiobuttons = quizRB;
+
+            CheckBox[] quizCB = new CheckBox[] { (CheckBox)this.FindName("quizButton1"),
+                                                 (CheckBox)this.FindName("quizButton2"),
+                                                 (CheckBox)this.FindName("quizButton3"),
+                                                 (CheckBox)this.FindName("quizButton4")};
+            checkbuttons = quizCB;
         }
 
         public void SerializeQuestions()
         {
-            string[] quiz1options = { "Halo Infinite", "Call of Duty: Vanguard", "Battlefield 2042" };
+            string[] quiz1options = { "Halo Infinite", "Call of Duty: Vanguard", "Battlefield 2042", "Back4Blood" };
             string[] quiz1answers = { "Halo Infinite", "Call of Duty: Vanguard" };
             Quiz quiz1 = new Quiz(2, "check", "Какие беты не разочаровали в 2021?", quiz1options, quiz1answers);
 
-            string[] quiz2options = { "1", "2", "0" };
+            string[] quiz2options = { "1", "2", "0", "Бесконечность" };
             string[] quiz2answers = { "1" };
             Quiz quiz2 = new Quiz(1, "radio", "Сколько тут правильных ответов?", quiz2options, quiz2answers);
 
             List<Quiz> questions = new List<Quiz>();
-            questions.Add(quiz1);
             questions.Add(quiz2);
+            questions.Add(quiz1);
 
             //Сериализация списка
             XmlSerializer serializer = new XmlSerializer(questions.GetType(), new XmlRootAttribute("Quiz"));
@@ -60,15 +84,22 @@ namespace RtfImportTest
             writer.Close();
         }
 
-        public void DeserializeQuestions()
+        public void UpdateQuiz()
         {
-            //Десериализация списка 
-            List<Quiz> returnedQuestions = new List<Quiz>();
-            using (var reader = new StreamReader("quiz.xml"))
+        }
+
+        public void SwitchQuiz()
+        {
+            currentQuizId++;
+            UpdateQuiz();
+
+            if (currentQuiz.QuizType == "radio")
             {
-                XmlSerializer deserializer = new XmlSerializer(returnedQuestions.GetType(),
-                    new XmlRootAttribute("Quiz"));
-                returnedQuestions = (List<Quiz>)deserializer.Deserialize(reader);
+
+                radiobuttons[0].Content = currentQuiz.Answers[0];
+                radiobuttons[1].Content = currentQuiz.Answers[1];
+                radiobuttons[2].Content = currentQuiz.Answers[2];
+                radiobuttons[3].Content = currentQuiz.Answers[3];
             }
         }
 
